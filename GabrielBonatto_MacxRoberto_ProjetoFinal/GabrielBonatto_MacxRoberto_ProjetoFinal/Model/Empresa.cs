fa6ba2bcd6b65025ca270ac10828bb7983534e02
Macx-Roberto <macxizinhocesconetto@hotmail.com>
2019-11-02 19:28:45 -0300

    Cadastro de empresa

--- GabrielBonatto_MacxRoberto_ProjetoFinal/GabrielBonatto_MacxRoberto_ProjetoFinal/Model/Empresa.cs
+++ GabrielBonatto_MacxRoberto_ProjetoFinal/GabrielBonatto_MacxRoberto_ProjetoFinal/Model/Empresa.cs
@@ -6,5 +6,32 @@ using System.Threading.Tasks;
 
 namespace GabrielBonatto_MacxRoberto_ProjetoFinal.Model {
   class Empresa {
-  }
+        public int EmpresaId { get; set; }
+        public String RazaoSocial { get; set; }
+        public String CNPJ { get; set; }
+        public String Fone { get; set; }
+
+        public Empresa(int empresaId, string razaoSocial, string cNPJ, string fone)
+        {
+            EmpresaId = empresaId;
+            RazaoSocial = razaoSocial;
+            CNPJ = cNPJ;
+            Fone = fone;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var empresa = obj as Empresa;
+            return empresa != null &&
+                   EmpresaId == empresa.EmpresaId &&
+                   RazaoSocial == empresa.RazaoSocial &&
+                   CNPJ == empresa.CNPJ &&
+                   Fone == empresa.Fone;
+        }
+
+        public override int GetHashCode()
+        {
+            return 680593949 + EmpresaId.GetHashCode();
+        }
+    }
 }
