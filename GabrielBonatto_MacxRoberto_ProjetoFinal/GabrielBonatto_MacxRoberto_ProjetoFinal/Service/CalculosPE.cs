94db3f0ddebb3c8c6afc3bd1a3bd5fc23cd22034
Macx-Roberto <macxizinhocesconetto@hotmail.com>
2019-11-02 22:40:33 -0300

    Adicionados calculo do ponto de equilibrio

--- GabrielBonatto_MacxRoberto_ProjetoFinal/GabrielBonatto_MacxRoberto_ProjetoFinal/Service/CalculosPE.cs
+++ GabrielBonatto_MacxRoberto_ProjetoFinal/GabrielBonatto_MacxRoberto_ProjetoFinal/Service/CalculosPE.cs
@@ -7,20 +7,27 @@ using System.Threading.Tasks;
 
 namespace GabrielBonatto_MacxRoberto_ProjetoFinal.Service
 {
-    class CalculosPe
-    {
-        private Simulacoes simulacoes;
+  class CalculosPe
+  {
+    private Simulacoes simulacoes;
 
-        public CalculosPe(Simulacoes simulacoes)
-        {
-            this.simulacoes = simulacoes;
-        }
-
-        public void CalcularEquilibrio()
-        {
-            //simulacoes.
-            double custoTotal = simulacoes.CustoFixoTotal + (simulacoes.CustoVarUnit * simulacoes.QtdeVendaFinal);
-        }
-
+    public CalculosPe(Simulacoes simulacoes) {
+      this.simulacoes = simulacoes;
     }
+
+    public Double CalcularEquilibrio() {
+      return simulacoes.CustoFixoTotal + (simulacoes.PrecoUnit - simulacoes.CustoVarUnit );
+    }
+
+    public Double CalcularReceitaEquilibrio() {
+      return simulacoes.PrecoUnit * CalcularEquilibrio();
+    }
+
+    public Double CalcularCustoEquilibrio() {
+      return simulacoes.CustoFixoTotal + (simulacoes.CustoVarUnit * CalcularEquilibrio());
+    }
+
+
+
+  }
 }
